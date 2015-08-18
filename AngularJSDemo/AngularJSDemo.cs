594f8df56569f5d4892083a1860bd22e4f530628
Fabrício Zimmerer Murta <fabricio@object.net>
2015-08-18 15:19:45 -0300

    Creates initial project.

---
+++ AngularJSDemo/AngularJSDemo.cs
@@ -0,0 +1,109 @@
+﻿using Bridge;
+using Bridge.AngularJS;
+using Bridge.Html5;
+using System;
+using System.Collections.Generic;
+
+namespace AngularJSDemo
+{
+    /// <summary>
+    /// Checkpoints:
+    /// Alpha Bravo Charlie Delta Echo Foxtrot Golf Hotel India
+    /// Juliet Kilo Lima Mike November Oscar Papa Quebec Romeo
+    /// Sierra Tango Uniform Victor Whiskey X-ray Yankee Zulu
+    /// http://home.earthlink.net/~malcolmhamer/alpha.htm
+    /// </summary>
+    public class AngularJSDemo
+    {
+
+        private class ControllerDataObjectStructure
+        {
+            public string Message { get; set; }
+            public string Foo { get; set; }
+            public string Bar { get; set; }
+        }
+
+        static AngularJSDemo()
+        {
+            Console.Log("Checkpoint Yankee: [ctor] nothing at this point will like AngularJS.");
+        }
+
+        public static AngularJSApp hwbApp;
+
+        /// <summary>
+        /// AngularJS requires to be defined on document parse.
+        /// TODO: create an onParse attribute
+        /// </summary>
+        public static void StartUp()
+        {
+            Console.Log("Checkpoint Charlie: I must be printed BEFORE Checkpoint 1.");
+
+            AngularJSDemo.hwbApp = new AngularJSApp("hwbApp");
+
+            var controllerData = new { message = "Hello, AngularJS message defined in Bridge's C#! :D" };
+            AngularJSDemo.hwbApp.Controller("hwbctl", controllerData);
+            AngularJSDemo.hwbApp.Directive("brdEntryPoint", AngularJSDemo.dynMehTemplate);
+
+            var controllerStrongData = new ControllerDataObjectStructure()
+            {
+                Message = "Hello, AngularJS message defined in Bridge's C#'s strongly typed class! :D",
+                Foo = "Foo fighters from strong C#.",
+                Bar = "Strong C# 777 slot"
+            };
+            AngularJSDemo.hwbApp.Controller("hwbSctl", controllerStrongData);
+            AngularJSDemo.hwbApp.Directive("brdEntryPointForThree", AngularJSDemo.ThreeWayFunction);
+
+            Document.DocumentElement.setNGApp(AngularJSDemo.hwbApp);
+        }
+
+        /// <summary>
+        /// This function determines the format of the contents of the entry point where the dynamic
+        /// code will be injected.
+        /// </summary>
+        /// <returns></returns>
+        public static object dynMehTemplate()
+        {
+            var span = new SpanElement();
+            span.setNGController("hwbctl");
+            span.InnerHTML = "AJS says by directive: [{{message}}]";
+            return new { template = span.OuterHTML };
+        }
+        public static object ThreeWayFunction()
+        {
+            var span = new SpanElement();
+            span.setNGController("hwbSctl");
+            span.InnerHTML = "AJS for three scopined: [msg:{{Message}}][foo:{{Foo}}][bar:{{Bar}}]";
+            return new { template = span.OuterHTML };
+        }
+
+        // This one must be called after the tag has been defined in the HTML DOM, yet before
+        // HTML DOM has been loaded (DOMContentLoaded)
+        public static void StartUpIdPhase()
+        {
+            if (AngularJSDemo.hwbApp == null) {
+                StartUp();
+            }
+
+            var x = Document.GetElementById("entryPoint");
+            if (x != null)
+            {
+                //x.SetAttribute("ng-controller", "hwbctl"); // same as below
+                x.setNGController("hwbctl");
+                x.InnerHTML = "AJS says by element ID: [{{message}}]";
+            }
+        }
+
+        /// <summary>
+        /// Trying to define AngularJS controller/app after DOM is loaded will not work.
+        /// Angular is supposed to be defined during the page composition, not after it is ready.
+        /// </summary>
+        [Ready]
+        public static void UpdateControls()
+        {
+            Console.Log("Checkpoint Zulu: [rdy] nothing at this point will like AngularJS.");
+
+            var lbl = new SpanElement() { InnerHTML = "We have Bridge script running." };
+            Document.Body.AppendChild(lbl);
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,58 @@
+AngularJS Demo using Bridge.NET
+
+To run this example, follow this walkthrought:
+
+1. Get Bridge.NET sources and demo in a same directory
+mkdir bridge
+cd bridge
+git clone https://www.github.com/bridgedotnet/Bridge
+git clone https://www.github.com/bridgedotnet/Builder
+git clone https://www.github.com/bridgedotnet/Frameworks
+
+2. Clone also the Demos directory, to the same directory Bridge repos were cloned into
+git clone https://www.github.com/bridgedotnet/Demos
+
+3. Switch Bridge and Frameworks repos to the AngularJS branch
+cd Bridge
+git checkout AngularJS
+cd ..
+cd Frameworks
+git checkout AngularJS
+cd ..
+
+4. Build Bridge, opening with VS 2013/2015 either:
+Bridge/Bridge.Frameworks.Builder.sln
+Bridge/Bridge.Frameworks.Builder.Testing.sln
+
+4.1. You need to rebuild the solution if you skipped step 1 (for example, 
+if you already had) the project cloned and built on your computer.
+
+5. Open Demos/AngularJS/AngularJSDemo/AngularJSDemo.sln
+
+6. Build the project (F6 key).
+6.1. After it finishes, you can refresh the 'WebInterface' project so it shows the
+     generated JavaScript files that, for convenience are auto deployed there.
+6.1.1. To refresh the view, click the 'Refresh' button on the Solution Explorer 
+       toolbar when you have the 'WebInterface' project selected.
+
+7. Run the project (without debugging! -- hotkey ctrl+F5) Your browser will open
+   in the test AngularJS page.
+
+Appendix 1: pure AngularJS page.
+ If you want to see a similar result with pure AngularJS code (no Bridge built code)
+run the project (ctrl+F5) while looking at the contents of the 'index-angular.html'
+file in the WebInterface project.
+
+Appendix 2: rebuilding bridge to refresh the project -- DLLs already in use
+ Because of a known limitation of the UsingTask mechanism used to build Bridge, the
+built DLLs become stuck once the client project (this) is built once.
+ As the project points directly to Bridge's DLLs inside the main project, you will
+be unable to rebuild bridge (if changed something) until you completely close the
+Visual Studio instance where the AngularJSDemo has been opened.
+ So, whenever you want to rebuild Bridge, ensure the Visual Studio window that
+opened AngularJSDemo project is closed.
+
+DISCLAIMER: The contents of the test files are just random strings and have
+            absolutely no useful value other than ensuring their content is
+			reflected on the resulting web page, resembling the containing
+			variables they were originated from.
