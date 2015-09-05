dc2709b4b23dbf65b41c4a8472dbca6ef06a8639
Fabrício Zimmerer Murta <fabricio@object.net>
2015-09-05 19:05:34 -0300

    Clean SimpleTest.cs

--- AngularJSDemo/SimpleTest.cs
+++ AngularJSDemo/SimpleTest.cs
@@ -2,41 +2,10 @@
 using System;
 using Bridge;
 
-namespace TestBed
+namespace SimpleTest
 {
 
     public static class TestClass
     {
-        public class ModelClass
-        {
-            public string SpringField;
-            public string SpringProp { get; set; }
-        }
-
-        public static void Main()
-        {
-            var flanders = new ModelClass();
-            flanders.SpringField = "homer";
-            flanders.SpringProp = "marjorie";
-
-            Script.Write("console.log('SpringField:'+flanders.SpringField);");
-            Script.Write("console.log('springField:'+flanders.springField);");
-            Script.Write("console.log('SpringProp:'+flanders.SpringProp);");
-            Script.Write("console.log('springProp:'+flanders.springProp);");
-
-            InternalMechanism<ModelClass>(ParFuncSample);
-        }
-
-        public static void InternalMechanism<T>(Action<T> parfunc)
-        {
-            // No matter what happens here, this is library-specific limitation
-            // and is not expressed thru bridge constraints.
-            Script.Write("var parm = {}");
-            Script.Write("parfunc(parm)");
-        }
-
-        public static void ParFuncSample(ModelClass param)
-        {
-        }
     }
 }
