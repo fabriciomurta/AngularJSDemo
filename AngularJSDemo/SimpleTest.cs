8e6b51759ac50c899261ddcc8b33342da3a918bc
Fabrício Zimmerer Murta <fabricio@object.net>
2015-09-06 01:37:45 -0300

    Presentation - Step 3: setup the model and controller.

--- AngularJSDemo/SimpleTest.cs
+++ AngularJSDemo/SimpleTest.cs
@@ -1,10 +1,27 @@
-﻿using Bridge.Html5;
+﻿using Bridge.AngularJS;
+using Bridge.Html5;
 using System;
 using Bridge;
 
 namespace SimpleTest
 {
+    public class TestModel
+    {
+        public string[] MyItems;
+    }
+
     public static class TestClass
     {
+        public static void StartUp()
+        {
+            var app = new AngularJSApp("MyApp");
+
+            app.Controller<TestModel>("MyCtl", TestControl);
+        }
+
+        public static void TestControl([Name("$scope")] TestModel scope)
+        {
+            scope.MyItems = new string [] { "First", "Second" };
+        }
     }
 }
