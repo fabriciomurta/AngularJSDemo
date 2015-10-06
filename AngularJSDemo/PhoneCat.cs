0585e1171213ce6123f404e839dfb1afec10a6c3
Fabrício Zimmerer Murta <fabricio@object.net>
2015-10-06 14:37:17 -0300

    Creates a custom filter, according to step 9 of phonecat tutorial.

--- AngularJSDemo/PhoneCat.cs
+++ AngularJSDemo/PhoneCat.cs
@@ -15,13 +15,34 @@ namespace PhoneCat
         }
 
         public static void Init() {
-            var app = Angular.Module("phonecatApp", new string[] { "ngRoute", "phonecatControllers" });
+            var appDepend = new string[]
+            {
+                "ngRoute",
+                "phonecatControllers",
+                "phonecatFilters"
+            };
+            var app = Angular.Module("phonecatApp", appDepend);
 
             app.Config<RouteProvider>(RouteProviderFn);
 
             var catCtl = Angular.Module("phonecatControllers");
             catCtl.Controller<PhoneListScopeModel, Http<PhoneModel[]>>("PhoneListCtrl", PhoneListCtrlFn);
             catCtl.Controller<PhoneDetailsScopeModel, PhoneModel, Http<PhoneDetailsModel>>("PhoneDetailCtrl", PhoneDetailCtrlFn);
+
+            var catFlt = Angular.Module("phonecatFilters");
+
+            // The following is equivalent to defining methods
+            // - string mb(string text) { return "sometext"; }
+            // and
+            // - Func<string, string> ma() { return mb; }
+            // Then calling .Filter("text", ma);
+            catFlt.Filter("checkmark", () =>
+            {
+                return (input) =>
+                {
+                    return (input == "true") ? "\u2713" : "\u2718";
+                };
+            });
         }
 
         public static void RouteProviderFn(
